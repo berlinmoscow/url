e46aa51476b049faf617b3858efe83a933a7100e
Sasha Kovalyov <alexander.kovaleff@gmail.com>
2014-05-19 10:48:20 +0200

    first commit

---
+++ url.js
@@ -0,0 +1,17 @@
+var url = 'http://github.com',
+    params = {
+      foo: 'foo',
+      bar: 'bar'
+    };
+
+module.exports = {
+
+  getUrl: function() {
+    return url;
+  },
+
+  getParams: function() {
+    return params;
+  }
+
+};
